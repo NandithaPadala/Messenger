dfa94bff190cd1be4dbc6993dbf06199aa36198b
NandithaPadala <padalanandihta@gmail.com>
2025-04-20 10:34:16 +0530

    Backend JWT Token Complete

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoute from './route/user.route.js';
 const app = express();
 dotenv.config();
+app.use(express.json());
 
-
+app.use(userRoute)
 
 const PORT =  process.env.PORT || 5001;
 const URI = process.env.MONGODB_URI;
@@ -13,14 +15,13 @@ try{
     mongoose.connect(URI);
     console.log('MongoDB connected successfully');
 }catch(err){
-    console.error('MongoDB connection error:', err);    
+    console.error('MongoDB connection error:', err);
 }
 
 
-app.get('/',(req,res) => {
-    res.send('Hello World!')
-})
+app.use("/user", userRoute);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
+});
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import tailwindcss from '@tailwindcss/vite'; // Ensure the Vite plugin is used
-import react from '@vitejs/plugin-react'; // If you're using React
-import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite'; // Ensure the Vite plugin is used
+// import react from '@vitejs/plugin-react'; // If you're using React
+// import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 })
