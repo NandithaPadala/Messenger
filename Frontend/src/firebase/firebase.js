dc42ace11653b88c8268aacefc9f660360bca0bd
NandithaPadala <143520207+NandithaPadala@users.noreply.github.com>
2025-04-27 11:23:59 +0530

    Update firebase.js

--- Frontend/src/firebase/firebase.js
+++ Frontend/src/firebase/firebase.js
@@ -3,15 +3,5 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDXk2PgxrxujcsTvC_Ad6FGpzRo_DnAPLs",
-    authDomain: "messenger-89551.firebaseapp.com",
-    projectId: "messenger-89551",
-    storageBucket: "messenger-89551.firebasestorage.app",
-    messagingSenderId: "283653391405",
-    appId: "1:283653391405:web:15eda115d71de581e4e4a4"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
